40eee3509d6ac36c67fbe5cac55dc8c9b9fdac54
spensation <spencevans@gmail.com>
2020-02-10 15:59:47 -0500

    prime

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(n)
+	# first, check for n = 1 
+	return false if n < 2
+	# none? Passes each element of the collection 
+	# to the given block. The method returns 
+	# true if the block never returns true for 
+	# all elements. If the block is not given, 
+	# none? will return true only if none of the 
+	# collection members is true.
+	(2..n/2).none?{ |i| n % i == 0 }
+end
